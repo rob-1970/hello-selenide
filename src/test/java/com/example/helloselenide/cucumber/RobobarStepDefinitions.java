4f0273665f883f190d0e3039686b62dcff994268
rob-1970 <rob.unic.1970@gmail.com>
2022-02-10 10:45:38 +0100

    Add CUCUMBER #4 AGE TEST

--- src/test/java/com/example/helloselenide/cucumber/RobobarStepDefinitions.java
+++ src/test/java/com/example/helloselenide/cucumber/RobobarStepDefinitions.java
@@ -1,19 +1,28 @@
 package com.example.helloselenide.cucumber;
 
 import com.example.helloselenide.robobar.CartPage;
+import com.example.helloselenide.robobar.CheckOutPage;
+import com.example.helloselenide.robobar.OrderPage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RobobarStepDefinitions {
-    CartPage cartPage;
+    // AFEGIR en general la linia 24
+    private CartPage cartPage;
+    private CheckOutPage checkoutPage;
+    private OrderPage orderPage;
+
     @Given("user opens robobar website")
     public void userOpensRobobarWebsite() {
-        open("http//localhost:3000");
+        open("http://localhost:3000");
+        //open("http://localhost:3000");
+        // AFEGIR en general la linia 24
+        cartPage = new CartPage();
     }
 
     @When("user add a cola")
@@ -22,8 +31,49 @@ public class RobobarStepDefinitions {
         cartPage.addCola();
     }
 
+    @When("user add a beer")
+    public void userAddABeer() {
+        cartPage.addBeer();
+    }
+
     @Then("total should be €{double}")
     public void totalShouldBe€(Double total) {
-        cartPage.total().shouldBe(exactText("€"+total.toString()));
+        cartPage.total().shouldBe(exactText(String.format("€%.2f",total)));
+    }
+
+    @When("user press submit button")
+    public void userPressSubmitButton() {
+        cartPage.checkOut();
+        checkoutPage = new CheckOutPage();
+    }
+
+    @When("user enter her age is {int}")
+    public void userEnterHerAgeIs(int arg0) {
+        // checkoutPage.setAgeInput("17");       INICIALMENT funciona OK pero nomes per 17 o 20
+        // checkoutPage.setAgeInput("20");
+        checkoutPage.setAgeInput(String.valueOf(arg0));
+    }
+
+    @When("user press order button")
+    public void userPressOrderButton() {
+        checkoutPage.checkOut();
+        orderPage = new OrderPage();
+    }
+
+    @Then("alert is active")
+    public void alertIsActive() {
+        orderPage.getAlertAge().shouldNotBe(hidden);
+    }
+
+    @Then("alert is not active")
+    public void alertIsNotActive() {
+        orderPage.getAlertAge().shouldBe(hidden);
+    }
+
+    @And("order is confirmed")
+    public void orderIsConfirmed() {
+        //orderPage = checkoutPage.checkOut();
+        orderPage.getAlertAge().shouldBe(hidden);
+        orderPage.getOrderSent().shouldBe(text("Coming right up! ~bzzzt~"));
     }
 }
--- src/test/resources/com.example.helloselenide.cucumber/robobar.feature
+++ src/test/resources/com.example.helloselenide.cucumber/robobar.feature
@@ -10,3 +10,27 @@ Feature: Robobar cart
     When user add a cola
     And user add a cola
     Then total should be €2.50
+
+  Scenario: user add one beer
+    Given user opens robobar website
+    When user add a beer
+    Then total should be €2.00
+
+  Scenario: user add one beer and age is 17
+    Given user opens robobar website
+    When user add a beer
+    Then total should be €2.00
+    And user press submit button
+    And user enter her age is 17
+    And user press order button
+    Then alert is active
+
+  Scenario: user add one beer and age is 20
+    Given user opens robobar website
+    When user add a beer
+    Then total should be €2.00
+    And user press submit button
+    And user enter her age is 20
+    And user press order button
+    Then alert is not active
+    Then order is confirmed
